075b22419b22cffe606cfc49734836fa3cc8024d
sossoshihi147 <sossoshihi147@gmail.com>
2025-02-10 16:16:07 +0800

    Update 練習2終極密碼

---
+++ Assets/Scripts/practise_2_DaVinci_Code.cs
@@ -0,0 +1,62 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Oliver
+{
+    public class practise_2_Davinci_Code : MonoBehaviour
+    {
+        // 啟動判斷
+        private enum gameStart
+        {
+            Stop, Start
+        }
+        [SerializeField, Header("啟程")]
+        private gameStart START = gameStart.Stop;
+        [SerializeField, Header("請輸入1~100數字")]
+        public int Number;
+        public int Round;
+        public int randomInt;
+        private int oldNumber;
+        private void Awake()
+        {
+            randomInt = Random.Range(1, 101);
+            Number = 0;
+            Round = 0;
+            oldNumber = 101;
+
+        }
+        private void Update()
+        {
+            if ((int)START == 1)
+            {
+                while (randomInt != Number && Number >= 1 && oldNumber != Number)
+                {
+                    if (randomInt < Number)
+                    {
+                        Debug.Log("<color=#ff0> 你猜的數字太大了 </color>");
+                        oldNumber = Number;
+                        Round = Round + 1;
+                    }
+                    else if (randomInt > Number)
+                    {
+                        Debug.Log("<color=#ff0> 你猜的數字太小了 </color>");
+                        oldNumber = Number;
+                        Round = Round + 1;
+                    }
+                }
+                if (randomInt == Number)
+                {
+                    Debug.Log("<color=#ff0> --被你猜到啦-- </color>");
+                    Debug.Log($"<color=#0f0> ---總共用了{Round}回合--- </color>");
+                    oldNumber = 101;
+                    START = gameStart.Stop;
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
